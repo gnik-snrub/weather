1f83485d9221628202c8f6b9f3e403ce1365b0ac
Josiah Morris <josiahbmorris@hotmail.com>
2021-03-20 02:07:39 +1100

    Added function to search for weather data by city. Information goes to console.log

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './style.sass'
 
-console.log('test')
+async function getWeatherByCity(city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=79000ca5329e6de29a91aff757bde56a`, { mode: 'cors' })
+    if (response.status == 404) {
+        console.log('Error - City not found')
+    } else {
+        const weatherData = await response.json()
+        console.log(weatherData)
+    }
+}
